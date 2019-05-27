2661652699bd6de148be3510b43a7a0d18f6d1fe
zhenglongfei <i@merryyou.cn>
2019-05-27 15:49:37 +0800

    Initial commit

---
+++ rabbitma-api/src/main/java/com/niocoder/quickstart/Consumer.java
@@ -0,0 +1,48 @@
+package com.niocoder.quickstart;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        // 1. 创建ConnectionFactory
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setVirtualHost("/");
+
+        // 2. 通过链接工厂创建连接
+        Connection connection = factory.newConnection();
+
+        // 3. 通过connection 创建一个channel
+        Channel channel = connection.createChannel();
+
+        // 4. 创建一个队列
+        String queueName = "hello";
+        /**
+         *      * @param queue the name of the queue
+         *      * @param durable true if we are declaring a durable queue (the queue will survive a server restart)
+         *      * @param exclusive true if we are declaring an exclusive queue (restricted to this connection)
+         *      * @param autoDelete true if we are declaring an autodelete queue (server will delete it when no longer in use)
+         *      * @param arguments other properties (construction arguments) for the queue
+         */
+        channel.queueDeclare(queueName, true, false, false, null);
+
+        // 5. 创建消费者
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+
+        // 6. 设置channel
+        channel.basicConsume(queueName,true,consumer);
+
+        while (true){
+            // 7. 获取消息
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String msg = new String(delivery.getBody());
+            System.err.println("消费端: " + msg);
+        }
+    }
+}
---
+++ rabbitma-api/src/main/java/com/niocoder/quickstart/Procuder.java
@@ -0,0 +1,44 @@
+package com.niocoder.quickstart;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Procuder {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        // 1. 创建ConnectionFactory
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setVirtualHost("/");
+
+        // 2. 通过链接工厂创建连接
+        Connection connection = factory.newConnection();
+
+        // 3. 通过connection 创建一个channel
+        Channel channel = connection.createChannel();
+
+        // 4. 通过channel发送数据
+        for (int i = 0; i < 3; i++) {
+            String msg = "Hello World";
+            /**
+             *      * @param exchange the exchange to publish the message to
+             *      * @param routingKey the routing key
+             *      * @param props other properties for the message - routing headers etc
+             *      * @param body the message body
+             */
+            channel.basicPublish("", "hello", null, msg.getBytes());
+        }
+
+        // 5. 关闭连接
+        channel.close();
+        connection.close();
+    }
+}
